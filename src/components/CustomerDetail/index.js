d590f6ca83b1d3b3aedb991de6a148875f4687cc
poetries <jingguanliuye@gmail.com>
2017-12-20 00:36:13 +0800

    update

--- src/components/CustomerDetail/index.js
+++ src/components/CustomerDetail/index.js
@@ -1,89 +1,55 @@
 import React, { Component } from 'react'
-import { NavBar, Icon,Calendar,List,Switch} from 'antd-mobile';
+import { NavBar, Icon,WhiteSpace  } from 'antd-mobile';
+import { Tabs, Radio } from 'antd';
+import DatePicker from '@/components/DatePicker'
 import Chart from '@/components/Chart'
 
-const now = new Date();
-const extra = {
-  '2017/07/15': { info: 'Disable', disable: true },
-};
-extra[+new Date(now.getFullYear(), now.getMonth(), now.getDate() + 5)] = { info: 'Disable', disable: true };
-extra[+new Date(now.getFullYear(), now.getMonth(), now.getDate() + 6)] = { info: 'Disable', disable: true };
-extra[+new Date(now.getFullYear(), now.getMonth(), now.getDate() + 7)] = { info: 'Disable', disable: true };
-extra[+new Date(now.getFullYear(), now.getMonth(), now.getDate() + 8)] = { info: 'Disable', disable: true };
-Object.keys(extra).forEach((key) => {
-  const info = extra[key];
-  const date = new Date(key);
-  if (!Number.isNaN(+date) && !extra[+date]) {
-    extra[+date] = info;
-  }
-});
+const TabPane = Tabs.TabPane;
 export default class CustomerDetail extends Component {
   state = {
-    show: false
-  }
-  renderBtn() {
-    return (
-      <List.Item arrow="horizontal"
-        onClick={() => {
-          document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
-          this.setState({
-            show: true
-          });
-        }}
-      >
-        日期选择
-      </List.Item>
-    );
-  }
-
-
-  onConfirm = (startTime, endTime) => {
-    document.getElementsByTagName('body')[0].style.overflowY = this.originbodyScrollY;
-    this.setState({
-      show: false,
-      startTime,
-      endTime,
-    });
-  }
-
-  onCancel = () => {
-    document.getElementsByTagName('body')[0].style.overflowY = this.originbodyScrollY;
-    this.setState({
-      show: false,
-      startTime: undefined,
-      endTime: undefined,
-    });
-  }
+    mode: 'top',
+  };
   render() {
+    const { mode } = this.state;
     return (
       <div>
-            <NavBar
-                mode="dark"
-                icon={<Icon type="left" />}
-                onLeftClick={() => this.props.history.push(`/dashboard`)}
-                rightContent='广点通'
-                >{this.props.match.params.id}</NavBar>
-            
-            {/* <Chart /> */}
-
-            <List className="calendar-list" style={{ backgroundColor: 'white' }}>
-          {this.renderBtn('选择日期时间区间(快捷)', 'Select DateTime Range (Shortcut)', { pickTime: true, showShortcut: true })}
-         
-         
-          {this.state.startTime &&
-            <List.Item>Time1: {this.state.startTime.toLocaleString()}</List.Item>
-          }
-          {this.state.endTime &&
-            <List.Item>Time2: {this.state.endTime.toLocaleString()}</List.Item>
-          }
-        </List>
-        <Calendar
-          visible={this.state.show}
-          onCancel={this.onCancel}
-          onConfirm={this.onConfirm}
-        />
+          <NavBar
+            mode="dark"
+            icon={<Icon type="left" />}
+            onLeftClick={() => this.props.history.push(`/dashboard`)}
+            rightContent='广点通'
+            >{this.props.match.params.id}
+          </NavBar>
+          <DatePicker />
+          <WhiteSpace />
+          <Tabs
+            defaultActiveKey="1"
+            tabPosition={mode}
+            style={{ height: 220 }}
+            hideAdd={false}
+          >
+            <TabPane tab="曝光" key="1">1</TabPane>
+            <TabPane tab="点击量" key="2">2</TabPane>
+            <TabPane tab="点击率(%)" key="3">3</TabPane>
+            <TabPane tab="下载" key="4">4</TabPane>
+            <TabPane tab="下载成本" key="5">5</TabPane>
+            <TabPane tab="下载率(%)" key="6">6</TabPane>
+            <TabPane tab="曝光下载率(%)" key="7">7</TabPane>
+            <TabPane tab="激活" key="8">8</TabPane>
+            <TabPane tab="激活成本" key="9">9</TabPane>
+            <TabPane tab="激活率(￥)" key="10">10</TabPane>
+            <TabPane tab="注册" key="11">11</TabPane>
+            <TabPane tab="注册成本" key="12">12</TabPane>
+            <TabPane tab="注册率(%)" key="13">13</TabPane>
+            <TabPane tab="完件" key="14">14</TabPane>
+            <TabPane tab="完件成本" key="15">15</TabPane>
+            <TabPane tab="完件率(%)" key="16">16</TabPane>
+            <TabPane tab="授信" key="17">17</TabPane>
+            <TabPane tab="授信成本" key="18">18</TabPane>
+            <TabPane tab="授信率(%)" key="19">19</TabPane>
+        </Tabs>
+        <Chart />
       </div>
     )
   }
 }
-
